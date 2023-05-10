099938f7801c03bdc27d602820c1554a3a27c1e8
CAROLYNE-MELI <chepkorirmcarol@gmail.com>
2023-05-10 01:52:19 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @*s: string
+ * @s: string
  */
 void _puts_recursion(char *s)
 {
@@ -10,5 +10,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
 	_putchar('\n');
 }
