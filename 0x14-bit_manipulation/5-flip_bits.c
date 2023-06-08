e9e88fde1d053396c22679265c476a44f49a99ec
carolyn-meli <chepkorirmcarol@gmail.com>
2023-06-08 21:53:57 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int flip;
 
-	for (flip = 0; n || m; n >>= 1, m >>=1)
+	for (flip = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			flip++;
