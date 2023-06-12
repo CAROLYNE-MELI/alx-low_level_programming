66584d5dfd65fa9ce032d59d42ae4515d4743b23
carolyn-meli <chepkorirmcarol@gmail.com>
2023-06-12 18:51:18 +0300

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "main.h"
+
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -13,24 +15,26 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, _read, _write;
 	char *buffer;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
+	buffer = malloc(sizeof(char) *1024);
+	if (buffer == NULL)
+		return (0);
 	file_from = open(argv[1], O_RDONLY);
 	_read = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
 		if (file_from == -1 || _read == -1)
 		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
-		_write = write(to, buffer, _read);
+		_write = write(file_to, buffer, _read);
 		if (file_to == -1 || _write == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -38,12 +42,31 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(99);
 		}
-		_read = read(from, buffer, 1024);
+		_read = read(file_from, buffer, 1024);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (_read > 0);
 	free(buffer);
-	close_file(file_from);
-	close_file(file_to);
+	close(file_from);
+	close(file_to);
 	return (0);
 }
-
+/**
+ * error_file - checks for possibility of opening a file
+ * @file_to: to coppy to
+ * @file_from: copping from
+ * @argv: number of arguments
+ * Return: void
+ */
+void error_file(int file_to, int file_from, char *argv[])
+{
+	if (file_from ==  -1)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't write from file %s\n", argv[2]);
+		exit (99);
+	}
+}
