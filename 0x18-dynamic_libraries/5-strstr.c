09e33dbc59237f1027241ac30e3e957a12da62ff
carolyne meli <chepkorirmcarol@gmail.com>
2023-08-21 08:34:54 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strstr - Entry point
+* @haystack: input
+* @needle: input
+* Return: Always 0 (Success)
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *r = haystack;
+		char *p = needle;
+
+		while (*r == *p && *p != '\0')
+		{
+			r++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
